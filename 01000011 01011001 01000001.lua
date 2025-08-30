7d55c1f792f009f05db8cc75ca6db7516e5717a2
asideiameu <nte3494@gmail.com>
2025-08-30 20:46:08 -0300

    Update 01000011 01011001 01000001.lua

--- 01000011 01011001 01000001.lua
+++ 01000011 01011001 01000001.lua
@@ -8,8 +8,8 @@ local Window = Library:MakeWindow({
     Flags = "CYASTUDIOSHUB_Broookhaven"
 })
 Window:AddMinimizeButton({
-    Button = { Image = "rbxassetid://108662620575463", BackgroundTransparency = 0 },
-    Corner = { CornerRadius = UDim.new(35, 1) },
+    Button = { Image = "rbxassetid://113977525656869", BackgroundTransparency = 0 },
+    Corner = { CornerRadius = UDim.new(0, 6) },
 })
 
 local InfoTab = Window:MakeTab({ Title = "Info", Icon = "rbxassetid://15309138473" })
